a11d6f61d3252d6e9c1f8c2784e9b84a6a27cb71
KrzysiekZTM <krzysiek@zostawtonam.pl>
2018-05-21 09:00:02 +0200

    Initial setup

---
+++ admin/inc/admin_header.inc.php
@@ -0,0 +1,21 @@
+<?php
+include 'inc/admin_functions.inc.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $page_dsc; ?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/admin_style.css">
+    <title><?php echo $page_title; ?></title>
+  </head>
+  <body>
+
+    <!-- Main container for the page  -->
+    <div class="container">
+
+      <!-- Main row for the page -->
+      <div class="row">
---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// page title
+$page_title = 'Dashboard';
+$page_dsc = 'Lorem Ipsum';
+
+include 'inc/admin_header.inc.php' ?>
+
+<?php include 'inc/admin_footer.inc.php' ?>
